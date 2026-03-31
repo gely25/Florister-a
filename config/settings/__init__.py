c678b9c4bfe2b9a7fe853bc9a23fa3dabf314fa9
gely25 <angesegoviachavez@gmail.com>
2026-03-31 13:32:12 -0500

    feature:Final Deploy2

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,12 +1,4 @@
 import os
 
-# Determina qué archivo de configuración cargar
-# Por defecto usa 'pro' en servidores (Render) a menos que se especifique 'local'
-is_on_render = os.getenv('RENDER') == 'true'
-default_env = 'pro' if is_on_render else 'local'
-env_settings = os.getenv('DJANGO_SETTINGS_ENV', default_env)
-
-if env_settings == 'pro':
-    from .pro import *
-else:
-    from .local import *
+# Fuerza carga de configuraciones de producción siempre, sin nada de local
+from .pro import *
